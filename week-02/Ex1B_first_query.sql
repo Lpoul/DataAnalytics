1d8e1353f87b53ede72b067338c66642157642b6
Shizu <poulson.leon23@gmail.com>
2026-04-14 12:15:47 -0400

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,7 @@
 /*
 Sakila is a database used to collect information about film information and staff information for use by a film store.alter
+
+USE sakila;
+SELECT * FROM actor; -- Retrieved [200] records
 */
+select * from film; -- Retrieved [1000] records
