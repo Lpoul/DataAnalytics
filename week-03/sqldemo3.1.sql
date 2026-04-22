271ad715e5b2301e017a21e30517a155343f82b9
Shizu <poulson.leon23@gmail.com>
2026-04-22 13:09:42 -0400

    In-class Exercise

---
+++ week-03/sqldemo3.1.sql
@@ -0,0 +1,91 @@
+-- Leon Poulson
+-- April 20, 2026
+
+SHOW databases;
+USE northwind;
+SHOW TABLES;
+-- Example 1
+SELECT ProductName, UnitPrice
+FROM products;
+-- Example 2
+SELECT *
+FROM products;
+-- Example 3
+SELECT ProductName AS 'Product', UnitPrice AS 'Price(USD)', UnitsInStock AS 'Stock'
+FROM products;
+
+-- Example 4
+SELECT CompanyName, City, Country
+FROM customers
+	WHERE Country = 'Germany';
+
+-- Example 5
+SELECT ProductName, UnitPrice
+FROM products
+	WHERE UnitPrice > 50;
+
+-- Example 6
+SELECT OrderID, CustomerID, ShipCountry, Freight
+FROM orders
+	WHERE ShipCountry = 'France';
+
+-- Example 7
+SELECT ProductName, UnitsInStock,ReorderLevel
+FROM products
+	WHERE ReorderLevel > UnitsInStock;
+    
+-- Example 8
+SELECT OrderID, Freight
+FROM orders
+	WHERE Freight >= 100;
+
+-- Example 10
+SELECT ProductName, UnitPrice,UnitsInStock
+FROM products
+	WHERE UnitPrice > 20 AND UnitsInStock > 50;
+    
+-- Example 11
+SELECT CompanyName, Country
+FROM customers
+	WHERE Country = 'UK' or 'Ireland';
+    
+-- Example 12
+
+SELECT CategoryID, UnitPrice
+FROM products
+	WHERE (CategoryID = 1 OR 2) AND (UnitPrice < 20);
+
+-- Example 13
+SELECT CompanyName, Country
+FROM customers
+WHERE NOT Country = 'u.s.a' or'USA';
+
+-- Example 14
+SELECT ProductName
+FROM products
+WHERE NOT Discontinued;
+
+-- Example 15
+SELECT CompanyName,Country
+FROM customers
+WHERE Country IN ('Germany','France','Spain');
+
+-- Example 16
+SELECT ProductName,SupplierID
+FROM products
+WHERE SupplierID NOT IN ('1','2','3');
+
+-- Example 17
+SELECT ProductName, UnitPrice
+FROM products
+WHERE UnitPrice BETWEEN 10 AND 20;
+
+-- Example 19
+SELECT OrderID, CustomerID, ShipRegion
+FROM orders
+WHERE ShipRegion IS NULL;
+
+-- Example 20
+SELECT FirstName, LastName, Region
+FROM employees
+WHERE Region IS NOT NULL;
