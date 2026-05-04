cd644f457949fb0983845ded5f5890e49ce7c845
Shizu <poulson.leon23@gmail.com>
2026-05-04 15:04:16 -0400

    Exersise 1.B

--- week-05/Ex1b_variable_examples.py
+++ week-05/Ex1b_variable_examples.py
@@ -1,9 +1,11 @@
 customer_id = 0     #From my experience ID's are usually based on numbers
 customers_name = '' #Names contain letters and not numbers
 customer_gender = ''#Genders contain letters and not numbers
-customer_date_of_birth = 0 #this was tricky since date of birth can be typed out as month day and year i.e July fourteenth twenty twenty four, but I belive the MM/DD/YYYY format is more standard
+customer_date_of_birth = '' #String beacuse of the slashes in MM/DD/YYYY
 drivers_license_number = 0 #Says drivers license number and numbers don't contain letters
 auto_policy_number = 0 #Says Number and numbers don't contain letters
 
-
-
+firstname = 'Leon'
+nickname = 'Will'
+lastname = 'Poulson'
+date_of_birth = '07/19/2002'
