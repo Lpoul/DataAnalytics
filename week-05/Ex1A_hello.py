486a3bad302a8a3a4c9598957f9bed4dd7d5d04d
Shizu <poulson.leon23@gmail.com>
2026-05-04 12:48:44 -0400

    Exercise 1.A

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,27 @@
+# Hello world with print
+
 print('Hello World!')
+
+# Hello world with variables
+
+message = 'Hello World!'
+print(message)
+
+#Dollars and Cents exersie with numbers
+
+dollars = 3
+cents = .50
+
+print(dollars + cents)
+
+#The print does addition inside of it and removes the zero.
+
+cents = cents + .25
+print(dollars + cents)
+
+#Dollars and Cents in string
+
+d_str = '3 dollars'
+c_str = '50 cents'
+
+print(d_str + ' ' + c_str)
