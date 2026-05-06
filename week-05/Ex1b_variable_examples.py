52eb33b7302477f01715e8077feeb70a2dce9aa5
Shizu <poulson.leon23@gmail.com>
2026-05-06 12:10:09 -0400

    Completed Exersise 1.B

--- week-05/Ex1b_variable_examples.py
+++ week-05/Ex1b_variable_examples.py
@@ -10,3 +10,25 @@ nickname = 'Will'
 lastname = 'Poulson'
 date_of_birth = '07/19/2002'
 birth_city = 'Sulfolk,Va'
+ 
+ # a) What is the full list of reserved words that can’t be used for variable names?
+
+ # False      def       if        raise
+# None       del       import    return
+# True       elif      in        try
+# and        else      is        while
+# as         except    lambda    with
+# assert     finally   nonlocal  yield
+# break      for       not
+# class      from      or
+# continue   global    pass
+
+# b)  b) Pick 5 of these words and review the explanation for how it is used as a keyword in
+#        Python. Add these 5 definitions as # comments to your exercise document. Put ^^
+#        around any terms that you are not familiar with.
+
+# if - A statement that checks for conditionals
+# elif - A statement to check other conditionals that may pass if it doesnt satisfy the if statement and doesnt fit into the else
+# while - A type of loop that runnes until a condidtion is met
+# for - A type of loop that will run for preset amount of time
+# import - adds a libariry into python for you to use 
