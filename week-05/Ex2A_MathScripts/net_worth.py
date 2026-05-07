702ab2b67f4912ba89bfa2925f36905f5c1e5ab8
Shizu <poulson.leon23@gmail.com>
2026-05-07 12:32:27 -0400

    Net Worth Calulations

---
+++ week-05/Ex2A_MathScripts/net_worth.py
@@ -0,0 +1,28 @@
+# Name: Leon Poulson
+# Date: 5/7/26
+# Instructor: Deodat
+#Calulations for net-worth and the area of a rectangle
+
+
+
+# Might have to redo this with input statements and loops
+
+        # asset_ammount = 250000
+        # debt_ammount = 10000 
+        # net_worth = asset_ammount - debt_ammount
+
+        # print(f"Your total assets are {asset_ammount}")
+        # print(f"Your total debts are {debt_ammount}")
+        # print(f"Your net worth is {net_worth}")
+
+
+# Might want to redo this with an input statement asking for the birthdat month and day
+
+        # side_a = 7
+        # side_b = 19
+        # area_rectangle = side_a * side_b
+
+        # print(f"Side A is {side_a}")
+        # print(f"Side B is {side_b}")
+        # print(f"The area of the rectangle is {area_rectangle}")
+
