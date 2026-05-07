d41414b9b03fdd69d17e7f0499648b715f97cfc5
Shizu <poulson.leon23@gmail.com>
2026-05-07 12:32:57 -0400

    Resturant tip amount

---
+++ week-05/Ex2A_MathScripts/tip_amount.py
@@ -0,0 +1,10 @@
+# Name: Leon Poulson
+# Date: 5/7/26
+# Instructor: Deodat
+# Calulate the cost of a tip for a restaurant bill
+
+
+meal_cost = 500
+tip = meal_cost * 0.15
+print(f"The tip on a ${meal_cost} restaurant bill is ${tip:.2f}")
+
