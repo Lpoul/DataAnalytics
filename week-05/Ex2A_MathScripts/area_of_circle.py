018a726292d7de40e9c0e7cad92d28aadaf2adcb
Shizu <poulson.leon23@gmail.com>
2026-05-07 12:32:18 -0400

    Area of a Circle calulations

---
+++ week-05/Ex2A_MathScripts/area_of_circle.py
@@ -0,0 +1,11 @@
+# Name: Leon Poulson
+# Date: 5/7/26
+# Instructor: Deodat
+# Calulate the area of a circle
+
+import math
+
+radius = 240
+area = math.pi * radius**2
+
+print(f"The area of a circle with radius {radius} is {area}")
