f312e42ae43235b79969f769de75bb918c7b21b8
Shizu <poulson.leon23@gmail.com>
2026-05-13 12:51:33 -0400

    In-class exersise

---
+++ week-06/sales_summary.py
@@ -0,0 +1,18 @@
+associate_name, store_region, units_sold, price_per_unit = input("Please enter your name," \
+                                                                "store region," \
+                                                                "how many units sold,"
+                                                                "price per unit: ").split(",")
+name = str(associate_name)
+region = str(store_region)
+units_sold = int(units_sold)
+price_per_unit = float(price_per_unit)
+
+def sales_summary(name, region, units, price):
+    total_revenue = units * price
+    performance_bonus = total_revenue * 0.10
+
+    print(f"{name} has earned ${total_revenue} and gained a bouns of ${performance_bonus}")
+
+
+
+sales_summary(name,region,units_sold,price_per_unit)
