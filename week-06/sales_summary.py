2ffe4269ce685b4f8490dae7310c1d2daaddd62b
Shizu <poulson.leon23@gmail.com>
2026-05-14 14:07:53 -0400

    In-class exersise

--- week-06/sales_summary.py
+++ week-06/sales_summary.py
@@ -1,18 +1,21 @@
-associate_name, store_region, units_sold, price_per_unit = input("Please enter your name," \
-                                                                "store region," \
-                                                                "how many units sold,"
-                                                                "price per unit: ").split(",")
-name = str(associate_name)
-region = str(store_region)
-units_sold = int(units_sold)
-price_per_unit = float(price_per_unit)
+# associate_name, store_region, units_sold, price_per_unit = input("Please enter your name," \
+#                                                                 "store region," \
+#                                                                 "how many units sold,"
+#                                                                 "price per unit: ").split(",")
+# name = str(associate_name)
+# region = str(store_region)
+# units_sold = int(units_sold)
+# price_per_unit = float(price_per_unit)
 
-def sales_summary(name, region, units, price):
-    total_revenue = units * price
-    performance_bonus = total_revenue * 0.10
+# def sales_summary(name, region, units, price):
+#     total_revenue = units * price
+#     performance_bonus = total_revenue * 0.10
 
-    print(f"{name} has earned ${total_revenue} and gained a bouns of ${performance_bonus}")
+#     print(f"{name} has earned ${total_revenue} and gained a bouns of ${performance_bonus}")
 
 
 
-sales_summary(name,region,units_sold,price_per_unit)
+# sales_summary(name,region,units_sold,price_per_unit)
+import random
+fruits = [1,2,3,4,5,6,7,8,9,10]
+print(random.shuffle(fruits))
